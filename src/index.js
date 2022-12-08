160c5f02758cb524892cb6bffd5d077ab25489f9
adrian <adrianperezm.00@gmail.com>
2022-12-08 03:27:39 -0700

    chore: Add fastify swagger configuration

--- src/index.js
+++ src/index.js
@@ -2,21 +2,28 @@
 const fastify = require('fastify')({
     logger: true
 })
-
 const userRoutes = require('./routes/users/users.routes')
+const fastifySwagger = require('@fastify/swagger')
+const fastifySwaggerUi = require('@fastify/swagger-ui')
+
+const swaggerOptions = require('./utils/swagger')
+
+fastify.register(fastifySwagger, swaggerOptions.swaggerOptions)
+
+fastify.register(fastifySwaggerUi, swaggerOptions.swaggerUIOptions)
 
 fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
 })
 
+/**Create routes */
 userRoutes.forEach(route => { fastify.route(route) })
 
-/**
- * Run the server!
- */
+/** Start the server! */
 const start = async () => {
     try {
         await fastify.listen({ port: 3000 })
+
         fastify.log.info(`Server is running on port ${fastify.server.address().port}`)
     } catch (err) {
         fastify.log.error(err)
