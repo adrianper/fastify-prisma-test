5a8272cfec02843a0832104b7d6e18cefb3cd70c
Alfredo Hernandez <alfredo.hdez@hotmail.com>
2022-12-14 17:15:49 -0700

    feat: prueba de pr

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ fastify.register(fastifySwagger, swaggerOptions.swaggerOptions)
 fastify.register(fastifySwaggerUi, swaggerOptions.swaggerUIOptions)
 
 fastify.get('/', async (request, reply) => {
-    return { hello: 'Esta es una prueba de fastify' }
+    return { hello: 'Hola Mundo' }
 })
 
 /**Create routes */
